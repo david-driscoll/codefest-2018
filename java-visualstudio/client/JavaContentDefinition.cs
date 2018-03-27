12a65f1a264dc493293e9a89889acf8573ce47ad
David Driscoll <david.driscoll@gmail.com>
2018-03-26 22:44:09 -0400

    Updated folder names

---
+++ java-visualstudio/client/JavaContentDefinition.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.LanguageServer.Client;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace MockLanguageExtension
+{
+    public class JavaContentDefinition
+    {
+        [Export]
+        [Name("java")]
+        [BaseDefinition(CodeRemoteContentDefinition.CodeRemoteContentTypeName)]
+        internal static ContentTypeDefinition FooContentTypeDefinition;
+
+
+        [Export]
+        [FileExtension(".java")]
+        [ContentType("java")]
+        internal static FileExtensionToContentTypeDefinition FooFileExtensionDefinition;
+    }
+}
